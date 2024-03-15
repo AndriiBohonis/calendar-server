08f0ad9d6e83f1bb728f4751d060695e7e6c9e35
Andrii <andriy30534>
2024-03-15 20:36:12 +0200

    feat:added controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        
+        $user = auth()->user();
+    
+        return response()->json([
+        'token' => $token,
+        'user' => $user
+    ]);
+    }
+public function register(Request $request)
+{
+    // Валідація даних
+    $validatedData = $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|string|min:6',
+    ]);
+
+    // Створення нового користувача
+    $user = User::create([
+        'name' => $validatedData['name'],
+        'email' => $validatedData['email'],
+        'password' => Hash::make($validatedData['password']),
+    ]);
+
+
+    $token = auth()->login($user);
+
+
+      return response()->json([
+        'token' => $token,
+        'user' => $user
+    ]);
+}
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+
+{
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(){
+        $user_id = auth()->id();
+          $events = Event::where('user_id', $user_id)->get();
+
+       return  $events;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'description' => 'required',
+            'event_date' => 'required',
+            'done' => 'nullable',
+
+        ]);
+
+         $user_id = auth()->id();
+         $event = Event::create([
+            'description' => $request->description,
+            'event_date' => $request->event_date,
+            'user_id' => $user_id, 
+     ]);
+        return $event;
+    }
+
+   
+
+     public function update(Request $request, $id)
+{
+    $validatedData = $request->validate([
+        'done' => 'required|boolean', 
+    ]);
+
+    $event = Event::find($id);
+    $event->update(['done' => $validatedData['done']]);
+    return $event;
+}
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+      return Event::destroy($id);
+    }
+}
