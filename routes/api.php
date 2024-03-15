2b3d3a6fb563e0311f536009ea6cabd4b9e9f4fd
Andrii <andriy30534>
2024-03-15 20:37:47 +0200

    feat:added router

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EventController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::group (['namespace'=> 'Event','middleware' => 'jwt.auth'],function(){
+    Route::get('/events', [EventController::class, 'index']);
+    Route::post('/events', [EventController::class, 'store']);
+    Route::delete('/events/{id}', [EventController::class, 'destroy']);
+    Route::patch('/events/{id}/status', [EventController::class, 'update']);
+});
